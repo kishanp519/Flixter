a0a9a8210bfba4f5dd9bc00c3e46d3450cbe231d
Kishan Patel <kishanp519@gmail.com>
2021-01-27 16:21:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixter/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.flixter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rvMovies;
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rvMovies =
+    }
+}
---
+++ app/src/main/java/com/example/flixter/Movie.java
@@ -0,0 +1,8 @@
+package com.example.flixter;
+
+public class Movie {
+    private String id;
+    private String name;
+
+
+}
